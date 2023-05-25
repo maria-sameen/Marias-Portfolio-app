c5cdc5230f1b94c2095bb9e206c07dc893f26e63
Maria S <mariasameen123@hotmail.com>
2023-05-25 19:09:43 +0100

    adding another file in components file

---
+++ marias-portfolio/src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+        <h2>Projects</h2>
+         {/* Add your projects here */}
+    </section>
+  );
+};
+
+      
+      {/* Render project components dynamically here */}
+ 
+
+export default Projects;
